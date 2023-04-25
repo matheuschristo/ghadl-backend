a26a0027f410b4a1406678ac0d87ea5ab0c6ab81
Matheus Christo <matheuschristo.09@gmail.com>
2023-04-25 18:16:13 -0300

    Update SaleRepository.java

--- src/main/java/com/ghadl/repository/SaleRepository.java
+++ src/main/java/com/ghadl/repository/SaleRepository.java
@@ -1,5 +1,9 @@
 package com.ghadl.repository;
 
-public interface SaleRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ghadl.entity.SaleEntity;
+
+public interface SaleRepository extends JpaRepository<SaleEntity, Long>{
 
 }
