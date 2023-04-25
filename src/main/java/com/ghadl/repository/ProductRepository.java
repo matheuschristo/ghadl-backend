b6f1f5cc2fee31a9efc5c681d3c6cd3c99d6cb03
Matheus Christo <matheuschristo.09@gmail.com>
2023-04-25 18:16:15 -0300

    Update ProductRepository.java

--- src/main/java/com/ghadl/repository/ProductRepository.java
+++ src/main/java/com/ghadl/repository/ProductRepository.java
@@ -1,5 +1,9 @@
 package com.ghadl.repository;
 
-public interface ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ghadl.entity.ProductEntity;
+
+public interface ProductRepository extends JpaRepository<ProductEntity, Long>{
 
 }
