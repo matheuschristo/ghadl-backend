d3146e84afa257b39fdeae88928463e458bd314c
Matheus Christo <matheuschristo.09@gmail.com>
2023-04-25 19:20:14 -0300

    Update UserService and UserServiceImpl

--- src/main/java/com/ghadl/dto/user/UserDTO.java
+++ src/main/java/com/ghadl/dto/user/UserDTO.java
@@ -1,5 +1,5 @@
 package com.ghadl.dto.user;
 
-public record UserDTO() {
+public record UserDTO(Long id, String name, String email, String password) {
 
 }
--- src/main/java/com/ghadl/service/UserServiceImpl.java
+++ src/main/java/com/ghadl/service/UserServiceImpl.java
@@ -1,5 +1,53 @@
 package com.ghadl.service;
 
-public class UserServiceImpl {
+import org.springframework.stereotype.Service;
+
+import com.ghadl.dto.exception.UserEmailAlreadyRegisteredException;
+import com.ghadl.dto.exception.UserNotFoundException;
+import com.ghadl.dto.user.CreateUserDTO;
+import com.ghadl.dto.user.UserDTO;
+import com.ghadl.entity.UserEntity;
+import com.ghadl.mapper.UserMapper;
+import com.ghadl.repository.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	private final UserRepository repository;
+	private final UserMapper mapper;
+	
+	public UserServiceImpl(UserRepository repository, UserMapper mapper) {
+		this.repository = repository;
+		this.mapper = mapper;
+	}
+
+	public CreateUserDTO save(CreateUserDTO dto) {
+		
+		if(repository.existsByEmail(dto.email()))
+			throw new UserEmailAlreadyRegisteredException("Email "+ dto.email() +" is already registered.");
+		
+		UserEntity user = mapper.toEntity(dto);
+		UserEntity userSaved = repository.save(user);
+		
+		return mapper.toDTO(userSaved);
+	}
+
+	public void update(UserDTO dto, Long id) {
+		
+		UserEntity user = repository.findById(id).orElseThrow(() -> new UserNotFoundException("User "+ id +" was not found."));
+		
+		user.setName(dto.name());
+		user.setEmail(dto.email());
+		
+		repository.save(user);
+	}
+
+	public void delete(Long id) {
+		
+		if(!repository.existsById(id))
+			throw new UserNotFoundException("User "+ id +" was not found.");
+		
+		repository.deleteById(id);
+	}
 
 }
