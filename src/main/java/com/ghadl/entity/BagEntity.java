8264344dd387c7db1329c290c7a99424eb9fc4dc
Matheus Christo <matheuschristo.09@gmail.com>
2023-04-25 18:00:51 -0300

    Update BagEntity.java

--- src/main/java/com/ghadl/entity/BagEntity.java
+++ src/main/java/com/ghadl/entity/BagEntity.java
@@ -1,5 +1,79 @@
 package com.ghadl.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "bag")
 public class BagEntity {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "bag_id")
+	private Long id;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "user_id")
+	private UserEntity user;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name = "product_id")
+	private List<ProductEntity> products = new ArrayList<>();
+	
+	public BagEntity() {}
+
+	public BagEntity(Long id, UserEntity user) {
+		this.id = id;
+		this.user = user;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, products, user);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BagEntity other = (BagEntity) obj;
+		return Objects.equals(id, other.id) && Objects.equals(products, other.products)
+				&& Objects.equals(user, other.user);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public UserEntity getUser() {
+		return user;
+	}
+
+	public void setUser(UserEntity user) {
+		this.user = user;
+	}
+
+	public List<ProductEntity> getProducts() {
+		return products;
+	}
 }
